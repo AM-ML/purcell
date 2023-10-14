1d301233c24913bceb2f777d37073a025e64bc71
SoftEngineer-0 <aliredamoumneh@gmail.com>
2023-10-14 09:58:21 +0300

    StringBuilder + formatted Strings

--- src/App.java
+++ src/App.java
@@ -1,42 +1,41 @@
-import java.util.ArrayList;
-class Thing {
-    public String name;
-    public String description;
-    Thing (String name, String description) {
-        this.name = name;
-        this.description = description;
-    }
-
-    public void show () {
-        System.out.println(this.name + ": " + this.description);
-    }
-}
-
-class Things {
-    public ArrayList<Thing> things = new ArrayList<>();
-
-    public void add(Thing thing){
-        things.add(thing);
-    }
-
-    public void show(){
-        for (Thing thing : things) {
-            System.out.println(thing.name + ": " + thing.description);
-        }
-    }
-}
-
-
 public class App {
     public static void main(String[] args) throws Exception {
-        Thing thing1 = new Thing("Milk", "Comes from cows... or other animals.");
-        Thing thing2 = new Thing("Water", "Essential for living.");
+      //* [//*] -> are the actual steps that run while compiling
+      //?   Memory Consuming
+        
+            String s = "";
+            
+      //*  String temp = "String 1.";
+      //*  s = s + temp; 
 
-        Things things = new Things();
+            s += "String 1.";    
+            
+      //*  String temp2 = "String 2.";
+      //*  s = s + temp2;
+
+            s += "String 2.";
+            
+      //*  String temp3 = "String 3.";
+      //*  s = s + temp3;
+      
+            s += "String 3."; 
+
+        //? Not as memory consuming
+
+        StringBuilder st = new StringBuilder("");
+
+        st.append("String 1.");
+        st.append("String 2.");
+        st.append("String 3.");
         
-        things.add(thing1);
-        things.add(thing2);
-        
-        things.show();
+        StringBuilder str = new StringBuilder("");
+        str.append("String 1.")
+           .append("String 2.")
+           .append("String 3.");
+
+        System.out.println(s);
+        System.out.println(st);
+        System.out.println(str);
+        System.out.printf("formatted output: %20.0f, %.1f, %.2f", 1.2, 3.4, 5.6); // printf(formatted, args..);
     }
 }
