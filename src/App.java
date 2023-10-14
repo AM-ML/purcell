768556b03a84a0deb291a015e521dfdd6a4ea63c
SoftEngineer <aliredamoumneh@gmail.com>
2023-10-14 07:27:41 +0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,11 @@
+class Robot {
+    public void speak() {
+        System.out.println("Hello");
+    }
+}
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
