2d410cbd095438755c833b2097a39c541541e539
SoftEngineer-0 <aliredamoumneh@gmail.com>
2023-10-14 08:34:10 +0300

    Robot class (scanner + func params) #2

--- src/App.java
+++ src/App.java
@@ -1,11 +1,22 @@
+import java.util.Scanner;
+
 class Robot {
-    public void speak() {
-        System.out.println("Hello");
+    public void speak(String x) {
+        System.out.println("rob says: " + x);
+        System.out.println("    < ------- >");
+        System.out.println("     | o   o |");
+        System.out.println("     |   ||  |");
+        System.out.println("     | \\___/ |");
+        System.out.println("      -------");
     }
 }
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Robot rob = new Robot();
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Talk to the robot: ");
+        String resp = scan.nextLine();
+        rob.speak(resp);
     }
 }
